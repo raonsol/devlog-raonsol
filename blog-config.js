7abc3e1e17187f4ea2e7449225aa2348173d7e48
Byeongsu Kang <raonsol@kakao.com>
2022-08-16 21:06:26 +0900

    ADD: Initial commit

---
+++ blog-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: "raonsol's devlog",
+  description: "raonsol's devlog",
+  author: "raonsol",
+  siteUrl: "https://blog.raonsol.dev/",
+  links: {
+    github: "https://github.com/raonsol",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    email: "mailto:raonsol@kakao.com",
+  },
+  utterances: {
+    repo: "raonsol/gatsby-starter-raonsol",
+    type: "pathname",
+  },
+}
